66ab6bf7e223647aee2cea4e60ed737760d09edb
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2020-05-08 22:56:34 +0200

    Word Count bug fixed

--- exercises/WordCountK8s.py
+++ exercises/WordCountK8s.py
@@ -15,17 +15,14 @@ inputFile = "hdfs://hadoop-hadoop-hdfs-nn:9000/data/ghEmployees.txt"
 # %%
 outputFile = "hdfs://hadoop-hadoop-hdfs-nn:9000/tmp/jwcsturm.txt"
 
-# %% [markdown]
-# main program
 
 # %%
-if __name__ == "__main__":
-    #create a SparkSession without local master and app name
-   spark = (SparkSession.builder.getOrCreate())
-   # read file 
-   spark.sparkContext.setLogLevel("ERROR")
-   input = spark.sparkContext.textFile(inputFile)
-   counts = input.flatMap(lambda line : line.split(" ")).map(lambda word : [word, 1]).reduceByKey(lambda a, b : a + b)
+#create a SparkSession without local master and app name
+spark = (SparkSession.builder.getOrCreate())
+# read file 
+spark.sparkContext.setLogLevel("ERROR")
+input = spark.sparkContext.textFile(inputFile)
+counts = input.flatMap(lambda line : line.split(" ")).map(lambda word : [word, 1]).reduceByKey(lambda a, b : a + b)
 
 
 # %%
