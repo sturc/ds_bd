3413b5007e329036ee602f1c37f9d9c78f826647
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2021-12-02 08:51:06 +0100

    pretty printing for confusion matrix added

--- helpers/data_prep_and_print.py
+++ helpers/data_prep_and_print.py
@@ -1,7 +1,8 @@
 """Helper functions to pyspark data analysis"""
 
+from pyspark.sql import Window
 from pyspark.sql.types import DoubleType
-from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import udf, col, monotonically_increasing_id, row_number
 
 
 def print_df(sprkDF,num_rows=0): 
@@ -30,7 +31,21 @@ def add_weight_col(dataframe, label_col='label', weight_col_name='classWeightCol
   weighted_dataframe = dataframe.withColumn(weight_col_name, calculate_weights_udf(col(label_col)))
   return weighted_dataframe
 
-
+def print_confusion_matrix(spark, conf_matrix):
+  """pretty printing the confusion matrix in the common format Row=Prediction Column=Label"""
+  #print (list(map(list, zip(*conf_matrix.toArray().tolist()))))
+  col_name_count = 0
+  df_conf = spark.createDataFrame(list(map(list, zip(*conf_matrix.toArray().tolist()))))
+  for col_name in df_conf.columns.copy():
+    df_conf = df_conf.withColumnRenamed(col_name,"Label_"+str(col_name_count))
+    col_name_count +=1
+  new_col_names = df_conf.columns.copy()
+  df_conf = df_conf.withColumn("monotonically_increasing_id", monotonically_increasing_id())
+  window = Window.orderBy(col('monotonically_increasing_id'))
+  df_conf = df_conf.withColumn('Prediction', row_number().over(window)-1)
+  new_col_names.insert(0,"Prediction")
+  df_conf = df_conf.select(new_col_names)
+  return print_df(df_conf)
 
 
 
